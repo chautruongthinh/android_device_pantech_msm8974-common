d406fad6a07e5cd21257275630a761cc04a815e2
hoalamha <hoalamha@gmail.com>
2016-09-26 01:25:10 +0700

    Revert "ef63: update kernel source"
    This reverts commit 5d320add185b5f596704a55b8564b14875f963c7.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
 BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := cyanogenmod_ef63_defconfig
-TARGET_KERNEL_SOURCE := kernel/pantech/ef63
+TARGET_KERNEL_SOURCE := kernel/pantech/msm8x74
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # ANT+
