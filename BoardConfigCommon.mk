9f6fc2675a5cab5f1710e655936754c488774f3d
Chau Truong Thinh <chautruongthinh@gmail.com>
2017-12-14 10:43:16 +0700

    msm8974-common: Fix offline charger

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,7 +73,9 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 TARGET_HAS_LEGACY_CAMERA_HAL1 := true
 
 # Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # CM Hardware
 BOARD_HARDWARE_CLASS += $(COMMON_PATH)/cmhw
