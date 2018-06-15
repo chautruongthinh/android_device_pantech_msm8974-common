fb5ac613682bc219567dadfaa261da2fd1ad37c6
Chau Truong Thinh <chautruongthinh@gmail.com>
2018-06-15 13:49:32 +0700

    msm8974: set BOARD_KERNEL_IMAGE_NAME
    Change-Id: I80f4920aea9626b1567ee1e28b017001ae8446ae

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,6 +38,7 @@ TARGET_CPU_VARIANT := krait
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=NULL,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 vmalloc=260M loglevel=0
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
