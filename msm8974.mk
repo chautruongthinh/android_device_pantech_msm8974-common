3528c5accbd379478176528fa9d52b283717f455
Chau Truong Thinh <chautruongthinh@gmail.com>
2018-06-15 14:21:30 +0700

    msm8974: Add basic USB HAL that reports no status change
    Change-Id: Id118305a58286d6f67b30202d586a95f5963d60f

--- msm8974.mk
+++ msm8974.mk
@@ -262,7 +262,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.pantech
 
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory
