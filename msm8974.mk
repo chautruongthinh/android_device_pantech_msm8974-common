03b24992c61591eb753db3dc38b35217f3533e89
Chau Truong Thinh <chautruongthinh@gmail.com>
2018-06-29 10:22:45 +0700

    msm8974: build gps HAL from source
    Change-Id: I787aa5e91b0cd810dfd3df63e494b889756a7d30

--- msm8974.mk
+++ msm8974.mk
@@ -126,9 +126,13 @@ PRODUCT_PACKAGES += \
     liboverlay
 
 # GPS
+PRODUCT_PACKAGES += \
+    gps.msm8974
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 
 # IPv6
