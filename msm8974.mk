51f95206dbee3eb75a950a6d518405cfd41bf0df
Chau Truong Thinh <chautruongthinh@gmail.com>
2018-07-01 15:16:41 +0700

    msm8974: build GPS HAL from source
    Change-Id: Idf85a243e556ddef32fb2a07105163e225042d08

--- msm8974.mk
+++ msm8974.mk
@@ -149,7 +149,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl 
+    android.hardware.gnss@1.0-impl \
+    gps.msm8974
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
