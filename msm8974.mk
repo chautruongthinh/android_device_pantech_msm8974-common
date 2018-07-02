954a89ad0524f3a2aefbd6fe39656bdcbd8fe748
Chau Truong Thinh <chautruongthinh@gmail.com>
2018-07-03 00:12:51 +0700

    msm8974-common: Ship RenderScript HAL
    Change-Id: Iaec92fef5c1d3417f448ba3042de618bd54ad251

--- msm8974.mk
+++ msm8974.mk
@@ -218,6 +218,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service-qti
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     libbson \
