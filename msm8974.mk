820ce60548cde67aabcca5fb24d426519558f201
Chau Truong Thinh <chautruongthinh@gmail.com>
2018-07-02 23:45:26 +0700

    msm8974-common: Remove no longer used hostapd configs
    Change-Id: I1d524a052dc55857fc83545170f129aa78767c08

--- msm8974.mk
+++ msm8974.mk
@@ -287,9 +287,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     dhcpcd.conf \
-    hostapd_default.conf \
-    hostapd.accept \
-    hostapd.deny \
     wpa_supplicant.conf \
     wpa_supplicant_overlay.conf \
     p2p_supplicant_overlay.conf
