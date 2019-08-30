1328df66ae55301d148e6fa68bb10809fc5b54c0
Daniel Jarai <jaraidaniel@gmail.com>
2019-08-30 18:43:46 +0700

    msm8974-common: Make the external camera provider ignore internal cameras
    * The external camera provider occupies our camera v4l2 nodes,
      potentially crashing the kernel driver and blocking the camera HAL.
      Unfortunately, there's no easy way to just disable it, so let's just
      tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: Daniel Jarai <jaraidaniel@gmail.com>

--- msm8974.mk
+++ msm8974.mk
@@ -124,6 +124,9 @@ PRODUCT_PACKAGES += \
     libshim_atomic \
     libshim_camera_parameters
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
+
 # Data
 PRODUCT_PACKAGES += \
     librmnetctl
