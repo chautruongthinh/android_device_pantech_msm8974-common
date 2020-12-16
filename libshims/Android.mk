1ca5898e47d1df33981167c1bd2935f585b670bd
Chau Truong Thinh <chautruongthinh@gmail.com>
2020-12-16 23:29:20 +0700

    msm8974-common: Convert all shims to blue print

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,46 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := atomic.cpp
-LOCAL_MODULE := libshim_atomic
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PROPRIETARY_MODULE := true
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := CameraParameters.cpp
-
-LOCAL_MODULE := libshim_camera_parameters
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_32_BIT_ONLY := true
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := libaudioclient_shim.cpp
-LOCAL_MODULE := libaudioclient_shim
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_SHARED_LIBRARIES := libaudioclient
-LOCAL_VENDOR_MODULE := true
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := libbinder_interface.cpp
-LOCAL_MODULE := libshim_binder
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_SHARED_LIBRARIES := libbinder libutils
-
-include $(BUILD_SHARED_LIBRARY)
+include $(call first-makefiles-under,$(call my-dir))
